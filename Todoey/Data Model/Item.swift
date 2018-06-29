94b04a426d3b8535a69645f9429f67493096e3bd
Jean-Marie Bordas <jm.bordas@digitappgency.com>
2018-06-29 11:09:21 +0200

    Convert to MVC design pattern

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,16 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Jean-Marie Bordas on 29/06/2018.
+//  Copyright © 2018 Jean-Marie Bordas. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    
+    var title: String = ""
+    var done: Bool = false
+    
+}
